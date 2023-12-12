4f2738a2ff00ce432b212d282eb67c01a810862b
CaledTarique <caled.98@hotmail.com>
2023-12-11 23:06:13 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ModalTasksComponent } from './modal-tasks/modal-tasks.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    ModalTasksComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modal-tasks/modal-tasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-tasks',
+  templateUrl: './modal-tasks.component.html',
+  styleUrls: ['./modal-tasks.component.css']
+})
+export class ModalTasksComponent {
+
+}
